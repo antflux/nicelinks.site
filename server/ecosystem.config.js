72af31d507e1ba7853c9a468fbbb3707fd706aae
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2020-09-06 01:31:54 +0800

    :sparkles: update ecosystem config

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'nicelinks',
-      script: 'index.js',
+      script: './src/index.js',
       // Options reference: http://pm2.keymetrics.io/docs/usage/application-declaration/
       exec_mode: 'cluster',
       instances: "max",
       autorestart: true,
       watch: false,
-      max_memory_restart: '2G',
+      max_memory_restart: '1G',
       env: {
         NODE_ENV: "development",
       },
